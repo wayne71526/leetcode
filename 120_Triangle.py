407f712e1366c3fef37c898b04a78688d9d78d35
Wayne <a0918964526@gmail.com>
2024-05-27 12:48:42 +0800

    add 120_Triangle.py

---
+++ 120_Triangle.py
@@ -0,0 +1,19 @@
+# solution
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = [[10**4]*len(triangle[i]) for i in range(n)]
+        dp[0][0] = triangle[0][0]
+
+        for i in range(1, n):
+            for j in range(len(triangle[i])):
+                if j == 0:
+                    dp[i][j] = dp[i-1][j] + triangle[i][j]
+                elif j == len(triangle[i]) - 1:
+                    dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+                else:    
+                    dp[i][j] = min(dp[i-1][j-1] + triangle[i][j], dp[i-1][j] + triangle[i][j])
+
+
+        return min(dp[-1])
+        
