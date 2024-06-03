ab81d5243e2807a566ec7774337ffd2bfd655cb9
Wayne <a0918964526@gmail.com>
2024-06-03 14:54:40 +0800

    add 72_Edit_Distance.py

---
+++ 72_Edit_Distance.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+
+        # minimum number of operations
+        dp = [[0] * (n+1) for _ in range(m+1)]
+
+        for i in range(1, m+1):     # word1[:i] to word2=''
+            dp[i][0] = i
+        
+        for j in range(1, n+1):
+            dp[0][j] = j            # word1='' to word2[:j]
+
+        
+        for i in range(1, m+1):     # word1[:i] to word2[:j]
+            for j in range(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + 1
+                    # (刪除, 取代, 新增)
+
+        return dp[-1][-1]
