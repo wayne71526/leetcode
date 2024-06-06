65f3de5e47c5c6f6f59fe21d9df210b145285890
Wayne <a0918964526@gmail.com>
2024-06-07 01:23:15 +0800

    add 155_Min_Stack.py

---
+++ 155_Min_Stack.py
@@ -0,0 +1,21 @@
+# solution
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minstack or self.minstack[-1] >= val:
+            self.minstack.append(val)
+
+    def pop(self) -> None:
+        if self.minstack[-1] == self.stack.pop():
+            del self.minstack[-1]
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
